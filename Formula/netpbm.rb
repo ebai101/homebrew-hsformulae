5598e33fc55b8fdb0848d3d26c904f7857c8a9f4
Ethan Bailey <ebailey256@gmail.com>
2020-12-12 15:09:09 -0500

    added netpbm

---
+++ Formula/netpbm.rb
@@ -0,0 +1,78 @@
+class Netpbm < Formula
+  desc "Image manipulation"
+  homepage "https://netpbm.sourceforge.io/"
+  # Maintainers: Look at https://sourceforge.net/p/netpbm/code/HEAD/tree/
+  # for stable versions and matching revisions.
+  url "https://svn.code.sf.net/p/netpbm/code/stable", revision: 3967
+  version "10.86.17"
+  license "GPL-3.0-or-later"
+  version_scheme 1
+  head "https://svn.code.sf.net/p/netpbm/code/trunk"
+
+  livecheck do
+    url "https://sourceforge.net/p/netpbm/code/HEAD/tree/stable/"
+    strategy :page_match
+    regex(/Release v?(\d+(?:\.\d+)+)/i)
+  end
+
+  bottle do
+    cellar :any
+    sha256 "50b44c9c75ca73b2b303b4c1706761d6856359712b4ed7dbfeb267c52f16828c" => :catalina
+    sha256 "143c5fc382df0e7cc34f5028b2cd394f9fbd79b78143666a1e6a336c178bc6f4" => :mojave
+    sha256 "6adaa802e331c0f92b36bdf7ce8c1df661f19b292869f1217317f092571c9c99" => :high_sierra
+  end
+
+  depends_on "jasper"
+  depends_on "jpeg"
+  depends_on "libpng"
+  depends_on "libtiff"
+
+  uses_from_macos "flex" => :build
+  uses_from_macos "libxml2"
+  uses_from_macos "zlib"
+
+  def install
+    # Fix file not found errors for /usr/lib/system/libsystem_symptoms.dylib and
+    # /usr/lib/system/libsystem_darwin.dylib on 10.11 and 10.12, respectively
+    ENV["SDKROOT"] = MacOS.sdk_path if MacOS.version == :sierra || MacOS.version == :el_capitan
+
+    cp "config.mk.in", "config.mk"
+
+    inreplace "config.mk" do |s|
+      s.remove_make_var! "CC"
+      s.change_make_var! "CFLAGS_SHLIB", "-fno-common"
+      s.change_make_var! "NETPBMLIBTYPE", "dylib"
+      s.change_make_var! "NETPBMLIBSUFFIX", "dylib"
+      s.change_make_var! "LDSHLIB", "--shared -o $(SONAME)"
+      s.change_make_var! "TIFFLIB", "-ltiff"
+      s.change_make_var! "JPEGLIB", "-ljpeg"
+      s.change_make_var! "PNGLIB", "-lpng"
+      s.change_make_var! "ZLIB", "-lz"
+      s.change_make_var! "JASPERLIB", "-ljasper"
+      s.change_make_var! "JASPERHDR_DIR", "#{Formula["jasper"].opt_include}/jasper"
+    end
+
+    ENV.deparallelize
+    system "make"
+    system "make", "package", "pkgdir=#{buildpath}/stage"
+
+    cd "stage" do
+      inreplace "pkgconfig_template" do |s|
+        s.gsub! "@VERSION@", File.read("VERSION").sub("Netpbm ", "").chomp
+        s.gsub! "@LINKDIR@", lib
+        s.gsub! "@INCLUDEDIR@", include
+      end
+
+      prefix.install %w[bin include lib misc]
+      lib.install Dir["staticlink/*.a"], Dir["sharedlink/*.dylib"]
+      (lib/"pkgconfig").install "pkgconfig_template" => "netpbm.pc"
+    end
+  end
+
+  test do
+    fwrite = shell_output("#{bin}/pngtopam #{test_fixtures("test.png")} -alphapam")
+    (testpath/"test.pam").write fwrite
+    system "#{bin}/pamdice", "test.pam", "-outstem", testpath/"testing"
+    assert_predicate testpath/"testing_0_0.", :exist?
+  end
+end
